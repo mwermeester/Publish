52eb72fa5df3d41d7e613c5cb41c8e3b3e578ed3
John Sundell <john@sundell.co>
2021-05-11 17:37:52 +0200

    Adopt Plot's new Component-based API (#111)
    - Bump Plot to version `0.9.0`.
    - Publish now ships with a few implementations of Plot's new `Component` protocol - specifically
       `Markdown` (for rendering Markdown inline within a component hierarchy), `VideoPlayer` (for
       rendering an inline video player), and an extension that makes it possible to directly use Plot's
      `AudioPlayer` component with Publish's `Audio` model.
    - The `Content.Body` type now also conforms to `Component`, which makes it possible to place
       such instances directly within a component hierarchy. That type has now also been fully documented,
       since it was previously missing documentation for some of its properties and initializers.
    - The built-in Foundation theme as been rewritten using the new component API. While it remains functionally
       identical to the previous implementation, it should act as a nice example of how this new API can be used.
    - Because Publish now ships with a type called `Markdown`, it's possible that some API users might need
       to disambiguate between this new type and Ink's `Markdown` type. However, that tradeoff was considered
       worth it, since using the new `Markdown` component will likely be a much more common use case.

--- Sources/Publish/API/PlotModifiers.swift
+++ Sources/Publish/API/PlotModifiers.swift
@@ -1,9 +1,12 @@
+<<<<<<< HEAD
 /**
 *  Publish
 *  Copyright (c) John Sundell 2021
 *  MIT license, see LICENSE file for details
 */
 
+=======
+>>>>>>> 8c23c5d (Adopt Plot's new Component-based API (#111))
 import Ink
 import Plot
 
