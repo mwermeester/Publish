a884a54f35de95f400768c711a1c81cf0c55f36a
Michael Wermeester <michael@wermeester.com>
2022-09-10 23:35:21 +0200

    Update to Swift Tools 5.6.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 
 /**
 *  Publish
@@ -16,41 +16,13 @@ let package = Package(
         .executable(name: "publish-cli", targets: ["PublishCLI"])
     ],
     dependencies: [
-        .package(
-            name: "Ink",
-            url: "https://github.com/johnsundell/ink.git",
-            from: "0.2.0"
-        ),
-        .package(
-            name: "Plot",
-            url: "https://github.com/johnsundell/plot.git",
-            from: "0.9.0"
-        ),
-        .package(
-            name: "Files",
-            url: "https://github.com/johnsundell/files.git",
-            from: "4.0.0"
-        ),
-        .package(
-            name: "Codextended",
-            url: "https://github.com/johnsundell/codextended.git",
-            from: "0.1.0"
-        ),
-        .package(
-            name: "ShellOut",
-            url: "https://github.com/johnsundell/shellout.git",
-            from: "2.3.0"
-        ),
-        .package(
-            name: "Sweep",
-            url: "https://github.com/johnsundell/sweep.git",
-            from: "0.4.0"
-        ),
-        .package(
-            name: "CollectionConcurrencyKit",
-            url: "https://github.com/johnsundell/collectionConcurrencyKit.git",
-            from: "0.1.0"
-        )
+        .package(url: "https://github.com/johnsundell/Ink.git", from: "0.5.1"),
+        .package(url: "https://github.com/johnsundell/Plot.git", from: "0.11.0"),
+        .package(url: "https://github.com/johnsundell/Files.git", from: "4.0.0"),
+        .package(url: "https://github.com/johnsundell/Codextended.git", from: "0.1.0"),
+        .package(url: "https://github.com/johnsundell/ShellOut.git", from: "2.3.0"),
+        .package(url: "https://github.com/johnsundell/Sweep.git", from: "0.4.0"),
+        .package(url: "https://github.com/johnsundell/CollectionConcurrencyKit.git", from: "0.1.0")
     ],
     targets: [
         .target(
